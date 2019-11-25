e9b74e842e3af7d1a0152f4efe497c08269bc7f3
Duy Van <duyvan1995@gmail.com>
2019-11-25 09:30:50 -0700

    Add Python Examples

---
+++ Python Server/serilization format.py
@@ -0,0 +1,28 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+#from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import serialization
+import base64
+import struct
+
+#generate server ECC key pair
+server_private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
+server_public_key = server_private_key.public_key()
+
+#Serializing the server public key
+serialized_public = server_public_key.public_bytes(
+    encoding=serialization.Encoding.X962, #PEM
+    format=serialization.PublicFormat.UncompressedPoint) #SubjectPublicKeyInfo)
+print(serialized_public[1:])
+
+#Serializing the server public key
+serialized_public = server_public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+public_key_hex = ((base64.b64decode(serialized_public.splitlines()[1]).hex()) + \
+                    (base64.b64decode(serialized_public.splitlines()[2]).hex()))[54:]
+public_key = b''
+for i in range(0,len(public_key_hex),2):
+    public_key += struct.pack("B",int(public_key_hex[i:i+2],16))
+print(public_key)
