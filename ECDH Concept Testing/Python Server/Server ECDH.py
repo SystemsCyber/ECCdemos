c4f0af200eeb363489b857ea20d58e53e17494e0
jeremy-daily <jeremy-daily@users.noreply.github.com>
2019-12-02 15:19:28 -0700

    Update Server ECDH.py

--- ECDH Concept Testing/Python Server/Server ECDH.py
+++ ECDH Concept Testing/Python Server/Server ECDH.py
@@ -1,13 +1,19 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+#from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import serialization
 import base64
-
+import struct
 
 #generate server ECC key pair
 server_private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
-server_public_key = (server_private_key.public_key())
+server_public_key = server_private_key.public_key()
+
+#Serializing the server public key
+serialized_public = server_public_key.public_bytes(
+	encoding=serialization.Encoding.X962, #PEM
+	format=serialization.PublicFormat.UncompressedPoint) #SubjectPublicKeyInfo)
+print(serialized_public[1:])
 
 #Serializing the server public key
 serialized_public = server_public_key.public_bytes(
@@ -16,6 +22,10 @@ serialized_public = server_public_key.public_bytes(
 
 public_key_hex = ((base64.b64decode(serialized_public.splitlines()[1]).hex()) + \
 					(base64.b64decode(serialized_public.splitlines()[2]).hex()))[54:]
+public_key = b''
+for i in range(0,len(public_key_hex),2):
+	public_key += struct.pack("B",int(public_key_hex[i:i+2],16))
+print(public_key)
 
 public_key_1 = public_key_hex[0:64]
 public_key_2 = public_key_hex[64:128]
