30793344595b94600db18d240d2c415d53adcaea
jeremy-daily <jeremy-daily@users.noreply.github.com>
2020-01-13 12:05:21 -0700

    Create ParsingCertificateExample.py

---
+++ ParsingCertificateExample.py
@@ -0,0 +1,27 @@
+#!/python 3
+
+import base64
+
+
+
+
+# https://www.amazontrust.com/repository/AmazonRootCA3.pem
+cert = """-----BEGIN CERTIFICATE-----
+MIIBtjCCAVugAwIBAgITBmyf1XSXNmY/Owua2eiedgPySjAKBggqhkjOPQQDAjA5
+MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6b24g
+Um9vdCBDQSAzMB4XDTE1MDUyNjAwMDAwMFoXDTQwMDUyNjAwMDAwMFowOTELMAkG
+A1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJvb3Qg
+Q0EgMzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABCmXp8ZBf8ANm+gBG1bG8lKl
+ui2yEujSLtf6ycXYqm0fc4E7O5hrOXwzpcVOho6AF2hiRVd9RFgdszflZwjrZt6j
+QjBAMA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgGGMB0GA1UdDgQWBBSr
+ttvXBp43rDCGB5Fwx5zEGbF4wDAKBggqhkjOPQQDAgNJADBGAiEA4IWSoxe3jfkr
+BqWTrBqYaGFy+uGh0PsceGCmQ5nFuMQCIQCcAu/xlJyzlvnrxir4tiz+OpAUFteM
+YyRIHN8wfdVoOw==
+-----END CERTIFICATE-----"""
+
+cert_lines = cert.split('\n')
+print(cert_lines)
+cert_encoded = ''.join(l for l in cert_lines[1:-1])
+print(cert_encoded)
+cert_bytes = base64.b64decode(cert_encoded)
+print(cert_bytes)
